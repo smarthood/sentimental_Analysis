abc1076c4b56b4c4c06784fc3a17970f3881c6a3
smarthood <renolddickson18@gmail.com>
2023-03-13 10:47:19 +0530

    dcd

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ def redirect(_url):
     st.markdown(link, unsafe_allow_html=True)
 st.title("Login")
 username=st.text_input('username', '')
-password=st.text_input('password', '')
+password=st.text_input('password', '',type='password')
 if st.button('SUBMIT'):
     if username=='admin' and password =='admin':
-        webbrowser.open("http://localhost:8502/main")
+        webbrowser.open("http://localhost:8501/main")
     else:
         st.write("Invalid user/password")
