aa1a18cecc83e6d9829e279cc33b71836e3c186f
smarthood <renolddickson18@gmail.com>
2023-03-21 13:01:21 +0530

    now

--- pages/main.py
+++ pages/main.py
@@ -1,5 +1,13 @@
 import json
 import streamlit as st
+# from yt_extractor import get_video_info, get_audio_url
+# from api_03 import save_transcript
+# from gtts import gTTS
+from pytube import extract
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+from textblob import TextBlob
+
 
 st.set_page_config(
     page_title="Sentimental Analysis",
@@ -8,47 +16,50 @@ st.set_page_config(
 )
 url = st.text_input('Enter video url', '')
 if st.button('SUBMIT'):
-    if 'youtube.com/' not in url:
+    if 'youtube.com' not in url:
         st.write("Invalid url")
     else:
         st.write(f'({url}) is currently proccessing....')
 
-def save_video_sentiments(url):
-    video_info = get_video_info(url)
-    url = get_audio_url(video_info)
-    if url:
-        title = video_info['title']
-        title = title.strip().replace(" ", "_")
-        title = "data/" + title
-        save_transcript(url, title, sentiment_analysis=True)
 
 if __name__ == "__main__":
-    # save_video_sentiments("https://youtu.be/e-kSGNzu0hM")
-    
-    with open("data/iPhone_13.json", "r") as f:
-        data = json.load(f)
-    
-    positives = []
-    negatives = []
-    neutrals = []
-    for result in data:
-        text = result["text"]
-        if result["sentiment"] == "POSITIVE":
-            positives.append(text)
-        elif result["sentiment"] == "NEGATIVE":
-            negatives.append(text)
-        else:
-            neutrals.append(text)
-        
-    n_pos = len(positives)
-    n_neg  = len(negatives)
-    n_neut = len(neutrals)
+    if (url != ""):
+        id = extract.video_id(url)
+        transcript = YouTubeTranscriptApi.get_transcript(id)
+        formatter = TextFormatter()
+        text_format = formatter.format_transcript(transcript)
+        file = open('transcript.txt', w)
+        file.write(text_format)
+        blob = TextBlob(text_format)
+        sentimental = blob.sentiment.polarity
+        st.write(sentimental)
+        print(sentimental)
 
-    print("Num positives:", n_pos)
-    print("Num negatives:", n_neg)
-    print("Num neutrals:", n_neut)
+        # audio = gTTS(text=text_format, lang="en", slow=False)
+        # audio.save("transcript.wav")
+        # save_transcript("transcript.wav", "senti_file", sentiment_analysis=True)
+        # with open("data/iPhone_13.json", "r") as f:
+        #     data = json.load(f)
+        # positives = []
+        # negatives = []
+        # neutrals = []
+        # for result in data:
+        #     text = result["text"]
+        #     if result["sentiment"] == "POSITIVE":
+        #         positives.append(text)
+        #     elif result["sentiment"] == "NEGATIVE":
+        #         negatives.append(text)
+        #     else:
+        #         neutrals.append(text)
 
-    # ignore neutrals here
-    r = n_pos / (n_pos + n_neg)
-    print(f"Positive ratio: {r:.3f}")
+        # n_pos = len(positives)
+        # n_neg  = len(negatives)
+        # n_neut = len(neutrals)
 
+        # print("Num positives:", n_pos)
+        # print("Num negatives:", n_neg)
+        # print("Num neutrals:", n_neut)
+
+        # # ignore neutrals here
+        # r = n_pos / (n_pos + n_neg)
+        # print(f"Positive ratio: {r:.3f}")
